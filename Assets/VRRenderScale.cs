c4c245f6ead3bd55d11a6dcce7a2273909aa8590
Ted Barnett <mail@tedbarnett.com>
2020-11-18 17:36:44 -0500

    Normcore 2.0.0 installed and working

---
+++ Assets/VRRenderScale.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.XR;
+
+// From https://yunhan.li/unity/2019/05/31/unity-oculus-standalone-graphic.html
+
+public class VRRenderScale : MonoBehaviour
+{
+
+
+    public float textureResolutionScale = 1.5f;
+    void Start()
+    {
+        XRSettings.eyeTextureResolutionScale = textureResolutionScale;
+    }
+}
