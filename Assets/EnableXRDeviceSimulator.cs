afb925d1b20051615683dd5200fb6d7f7cc67273
Ted Barnett <mail@tedbarnett.com>
2021-02-14 17:07:06 -0500

    Loading (but not tested on Oculus) under Unity 2020.2.4f1

---
+++ Assets/EnableXRDeviceSimulator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableXRDeviceSimulator : MonoBehaviour
+{
+    public GameObject XRDeviceSimulator;
+
+    void Awake()
+    {
+
+#if UNITY_EDITOR
+        Debug.Log("Running Unity Editor, so will enable XR Device Simulator");
+        XRDeviceSimulator.SetActive(true);
+#endif
+
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
